03086eb93ab457f39bb43e00720da96cc8ea98cd
bojote <perolita@gmail.com>
2016-05-24 10:32:47 -0600

    Works POrtfolio angular

---
+++ peopleFinder/peopleFinder/controllersl2.js
@@ -0,0 +1,17 @@
+
+var  myNameSpace= angular.module('myApp',[]);
+
+myNameSpace.controller('MyController', function MyController($scope){
+
+	$scope.author={
+		'nombre':'Leo Reyes',
+		'title':'ZOILA WEB',
+		'company':'GuarniCom',
+
+	}
+
+});
+
+
+
+	
